9be8c3c359aa7f62272d5127c0601febc4164418
George Appianing <george.adu.appianing@gmail.com>
2025-05-08 23:59:51 +0000

    added functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,89 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App({ URL }) {
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
 
-  const sortedList = todos.splice(0, 10);
+  const sortedList = todos.slice(0, 10);
 
-  function fetchTodos() {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => {
-        setTodos(data);
+  function fetchData() {
+    setLoading(true);
+    axios
+      .get(URL)
+      .then((response) => {
+        setTodos(response.data);
+        setLoading(false);
       })
-      .catch((err) => {
-        console.log("Error fetching todos:", err);
+      .catch((error) => {
+        console.log("Error message", error);
+        // setLoading(false);
       });
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input) {
+      return;
+    }
+    axios
+      .post(URL, { title: input, completed: false, id: todos.length + 1 })
+      .then(() => {
+        setTodos([...todos]);
+      });
+  }
+
+  function handleDelete(id) {
+    axios.delete(
+      `${URL}/${id}`.then(() => {
+        const updatedTodo = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodo);
+      })
+    );
+  }
+
   useEffect(() => {
-    fetchTodos();
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1 className="text-2xl">This is a Todo App </h1>
-      {sortedList.map((todo) => (
-        <div key={todo.id}>
-          <h2>Todo: {todo.title}</h2>
-        </div>
-      ))}
+      <form onSubmit={handleSubmit}>
+        <label>Enter Todo: </label>
+        <input
+          type="text"
+          className="ring-1"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="bg-blue-400 text-black" type="submit">
+          Submit
+        </button>
+      </form>
+      {!loading ? (
+        sortedList.map((todo) => (
+          <div key={todo.id}>
+            <h2>Todo: {todo.title}</h2>
+            <p>status: {todo.completed}</p>
+            <button
+              // onClick={() => handleDelete(todo.id)}
+              className="bg-red-300"
+            >
+              Delete
+            </button>
+            <button
+              // onClick={() => handleEdit(todo.id)}
+              className="bg-yellow-300"
+            >
+              Edit
+            </button>
+          </div>
+        ))
+      ) : (
+        <h2>Loading....</h2>
+      )}
     </div>
   );
 }
