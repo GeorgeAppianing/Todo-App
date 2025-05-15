91b17981ee3af1b18dd6db03f48b3284507eb585
George Appianing <george.adu.appianing@gmail.com>
2025-05-15 22:31:06 +0000

    updating UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ function App({ URL }) {
   const [priority, setPriority] = useState("");
   const [notes, setNotes] = useState("");
   const [category, setCategory] = useState("");
+  const [editTodo, setEditTodo] = useState(null);
 
-  const sortedList = todos.slice(0, 10);
+  // const sortedList = todos.slice(0, 10);
 
   function fetchData() {
     setLoading(true);
@@ -19,6 +20,7 @@ function App({ URL }) {
       .then((response) => {
         setTodos(response.data);
         setLoading(false);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log("Error message", error);
@@ -28,10 +30,11 @@ function App({ URL }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (!task || !status || !priority || !notes || !category) return;
+    if (!task || !status || !priority || !notes || !category)
+      return alert("Enter fields");
     setLoading(true);
     const updatedData = {
-      // id: todos.length + 1,
+      // id: todos.length + 1, this is not needed as the server will generate an id
       task,
       status,
       priority,
@@ -55,6 +58,71 @@ function App({ URL }) {
         setNotes("");
         setCategory("");
       });
+    console.log("submitted");
+  }
+
+  function handleDelete(id) {
+    axios
+      .delete(`${URL}/${id}`)
+      .then(() => {
+        const newTodo = todos.filter((todo) => todo.id !== id);
+        setTodos(newTodo);
+      })
+      .catch((error) => {
+        console.log(("Error message", error));
+      });
+  }
+
+  function handleEdit(id) {
+    const todoToEdit = todos.find((todo) => todo.id === id);
+    setTask(todoToEdit.task);
+    setStatus(todoToEdit.status);
+    setPriority(todoToEdit.priority);
+    setNotes(todoToEdit.notes);
+    setCategory(todoToEdit.category);
+    setEditTodo(id);
+
+    // setLoading(true);
+    const updatedData = {
+      task,
+      status,
+      priority,
+      notes,
+      category,
+    };
+    axios
+      .put(`${URL}/${id}`, updatedData)
+      .then((response) => {
+        const newTodo = todos.map((todo) =>
+          todo.id === id ? { ...todo, ...response.data } : todo
+        );
+        setTodos(newTodo);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error message", error);
+      })
+      .finally(() => {
+        setLoading(false);
+        setTask("");
+        setStatus("");
+        setPriority("");
+        setNotes("");
+        setCategory("");
+      });
+
+    // console.log("submitted");
+
+    // setLoading(false);
+    // setTask("");
+    // setStatus("");
+    // setPriority("");
+    // setNotes("");
+    // setCategory("");
+
+    // setEditTodo(null);
+
+    // setLoading(false);
   }
 
   useEffect(() => {
@@ -62,67 +130,78 @@ function App({ URL }) {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-2xl">This is a Todo App </h1>
-      <p>{todos.length}</p>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <label>Enter Title of Task:</label>
-        <input
-          type="text"
-          className="ring-1 p-2 w-full"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-
-        <label>Status</label>
-        <select
-          value={status}
-          onChange={(e) => setStatus(e.target.value)}
-          className="ring-1 p-2 w-full"
-        >
-          <option value="pending">Pending</option>
-          <option value="completed">Completed</option>
-        </select>
-
-        <label>Priority</label>
-        <select
-          value={priority}
-          onChange={(e) => setPriority(e.target.value)}
-          className="ring-1 p-2 w-full"
-        >
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
-        </select>
-
-        <label>Notes</label>
-        <textarea
-          value={notes}
-          onChange={(e) => setNotes(e.target.value)}
-          className="h-40 w-full ring-1 p-2 resize-none"
-        />
-
-        <label>Category</label>
-        <select
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          className="ring-1 p-2 w-full"
-        >
-          <option value="health">Health</option>
-          <option value="personal">Personal</option>
-          <option value="work">Work</option>
-          <option value="study">Study</option>
-          <option value="tech">Tech</option>
-          <option value="career">Career</option>
-          <option value="family">Family</option>
-          <option value="hobby">Hobby</option>
-          <option value="leisure">Leisure</option>
-          <option value="travel">Travel</option>
-          <option value="finance">Finance</option>
-        </select>
+    <div className="max-h-screen p-10 flex flex-col mx-auto w-3/4">
+      {/* <p>{todos.length}</p> */}
+      <form onSubmit={handleSubmit} className="">
+        <h1 className="text-2xl font-bold">📝 Task App </h1>
+        <div>
+          <label>Task</label>
+          <input
+            type="text"
+            className="ring-1 p-2 w-full"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Eg.Finish React project"
+          />
+        </div>
+        <div>
+          <label>Status</label>
+          <select
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+            className="ring-1 p-2 w-full"
+          >
+            <option value="">--Select Status--</option>
+            <option value="pending">Pending</option>
+            <option value="completed">Completed</option>
+          </select>
+        </div>
+        <div>
+          <label>Priority</label>
+          <select
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+            className="ring-1 p-2 w-full"
+          >
+            <option value="">--Select Priority--</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+        <div>
+          <label>Notes</label>
+          <textarea
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+            className="h-20 w-full ring-1 p-2 resize-none"
+            placeholder="Details about your task"
+          />
+        </div>
+        <div>
+          <label>Category</label>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="ring-1 p-2 w-full"
+          >
+            <option value="">Select Category</option>
+            <option value="health">Health</option>
+            <option value="personal">Personal</option>
+            <option value="work">Work</option>
+            <option value="study">Study</option>
+            <option value="tech">Tech</option>
+            <option value="career">Career</option>
+            <option value="family">Family</option>
+            <option value="hobby">Hobby</option>
+            <option value="leisure">Leisure</option>
+            <option value="travel">Travel</option>
+            <option value="finance">Finance</option>
+          </select>
+        </div>
 
         <button
-          className="bg-blue-400 hover:bg-blue-500 text-white px-4 py-2 rounded"
+          className="bg-blue-400 hover:bg-blue-500 text-white px-4 py-2 rounded "
           type="submit"
         >
           Submit
@@ -130,12 +209,12 @@ function App({ URL }) {
       </form>
 
       {!loading ? (
-        sortedList.map((todo) => (
-          <div key={todo.id}>
-            <h2>Todo: {todo.task}</h2>
+        todos.map((todo) => (
+          <div key={todo.id} className="border">
+            <h2>Title: {todo.task}</h2>
             <h2>status: {todo.status}</h2>
             <button
-              // onClick={() => handleDelete(todo.id)}
+              onClick={() => handleDelete(todo.id)}
               className="bg-red-300"
             >
               Delete
