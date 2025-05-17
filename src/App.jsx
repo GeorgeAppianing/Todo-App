1f835a132e231ea8b06fc01df3e65097df34bb20
George Appianing <george.adu.appianing@gmail.com>
2025-05-17 20:48:48 +0000

    Completed functionality and UI

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App({ URL }) {
   });
   const [editId, setEditId] = useState(null);
   const [formModal, setFormModal] = useState(false);
+  const [addFormModal, setAddFormModal] = useState(false);
   // Function for fetching Data
   function fetchData() {
     setLoading(true);
@@ -101,25 +102,11 @@ function App({ URL }) {
       })
       .finally(() => {
         setLoading(false);
-        setFormData({
-          task: "",
-          status: "",
-          priority: "",
-          notes: "",
-          category: "",
-        });
         setEditId(null);
       });
   }
   // function for populating form fields with existing Tasks
   function handleEditInput(todo) {
-    setFormData({
-      task: "",
-      status: "",
-      priority: "",
-      notes: "",
-      category: "",
-    });
     setFormModal(true);
     setFormData({
       task: todo.task,
@@ -151,87 +138,91 @@ function App({ URL }) {
   };
 
   return (
-    <div className=" p-10 flex flex-col mx-auto w-4/5">
+    <div className=" p-10 flex flex-col mx-auto w-4/5 ">
       <h1 className="text-2xl font-bold px-10">📝 Task App</h1>
-      <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4 p-10">
-        <div>
-          <label>Task</label>
-          <input
-            type="text"
-            className="ring-1 ring-gray-400 p-2 flex rounded w-full"
-            value={formData.task}
-            onChange={(e) => setFormData({ ...formData, task: e.target.value })}
-            placeholder="Eg.Finish React project"
+      {!addFormModal && (
+        <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4 p-10">
+          <div>
+            <label>Task</label>
+            <input
+              type="text"
+              className="ring-1 ring-gray-400 p-2 flex rounded w-full"
+              value={formData.task}
+              onChange={(e) =>
+                setFormData({ ...formData, task: e.target.value })
+              }
+              placeholder="Eg.Finish React project"
+            />
+          </div>
+          <div>
+            <label>Status</label>
+            <select
+              value={formData.status}
+              onChange={(e) =>
+                setFormData({ ...formData, status: e.target.value })
+              }
+              className="ring-1 ring-gray-400 p-2 flex rounded w-full"
+            >
+              <option value="">--Select Status--</option>
+              <option value="pending">Pending</option>
+              <option value="completed">Completed</option>
+            </select>
+          </div>
+          <div>
+            <label>Priority</label>
+            <select
+              value={formData.priority}
+              onChange={(e) =>
+                setFormData({ ...formData, priority: e.target.value })
+              }
+              className="ring-1 ring-gray-400 p-2 flex rounded w-full"
+            >
+              <option value="">--Select Priority--</option>
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+            </select>
+          </div>
+          <div>
+            <label>Category</label>
+            <select
+              value={formData.category}
+              onChange={(e) =>
+                setFormData({ ...formData, category: e.target.value })
+              }
+              className="ring-1 ring-gray-400 p-2 flex rounded w-full"
+            >
+              <option value="">Select Category</option>
+              <option value="health">Health</option>
+              <option value="personal">Personal</option>
+              <option value="work">Work</option>
+              <option value="study">Study</option>
+              <option value="tech">Tech</option>
+              <option value="career">Career</option>
+              <option value="family">Family</option>
+              <option value="hobby">Hobby</option>
+              <option value="leisure">Leisure</option>
+              <option value="travel">Travel</option>
+              <option value="finance">Finance</option>
+            </select>
+          </div>
+          <label>Notes</label>
+          <textarea
+            value={formData.notes}
+            onChange={(e) =>
+              setFormData({ ...formData, notes: e.target.value })
+            }
+            className="h-20 w-full ring-1 p-2 resize-none ring-gray-400 rounded col-span-full"
+            placeholder="Details about your task"
           />
-        </div>
-        <div>
-          <label>Status</label>
-          <select
-            value={formData.status}
-            onChange={(e) =>
-              setFormData({ ...formData, status: e.target.value })
-            }
-            className="ring-1 ring-gray-400 p-2 flex rounded w-full"
+          <button
+            className="bg-blue-700 hover:bg-blue-500 text-white px-2 py-1 rounded col-span-full"
+            type="submit"
           >
-            <option value="">--Select Status--</option>
-            <option value="pending">Pending</option>
-            <option value="completed">Completed</option>
-          </select>
-        </div>
-        <div>
-          <label>Priority</label>
-          <select
-            value={formData.priority}
-            onChange={(e) =>
-              setFormData({ ...formData, priority: e.target.value })
-            }
-            className="ring-1 ring-gray-400 p-2 flex rounded w-full"
-          >
-            <option value="">--Select Priority--</option>
-            <option value="high">High</option>
-            <option value="medium">Medium</option>
-            <option value="low">Low</option>
-          </select>
-        </div>
-        <div>
-          <label>Category</label>
-          <select
-            value={formData.category}
-            onChange={(e) =>
-              setFormData({ ...formData, category: e.target.value })
-            }
-            className="ring-1 ring-gray-400 p-2 flex rounded w-full"
-          >
-            <option value="">Select Category</option>
-            <option value="health">Health</option>
-            <option value="personal">Personal</option>
-            <option value="work">Work</option>
-            <option value="study">Study</option>
-            <option value="tech">Tech</option>
-            <option value="career">Career</option>
-            <option value="family">Family</option>
-            <option value="hobby">Hobby</option>
-            <option value="leisure">Leisure</option>
-            <option value="travel">Travel</option>
-            <option value="finance">Finance</option>
-          </select>
-        </div>
-
-        <label>Notes</label>
-        <textarea
-          value={formData.notes}
-          onChange={(e) => setFormData({ ...formData, notes: e.target.value })}
-          className="h-20 w-full ring-1 p-2 resize-none ring-gray-400 rounded col-span-full"
-          placeholder="Details about your task"
-        />
-
-        <button
-          className="bg-blue-700 hover:bg-blue-500 text-white px-2 py-1 rounded col-span-full"
-          type="submit"
-        >
-          Add Task
-        </button>
-      </form>
+            Add Task
+          </button>
+        </form>
+      )}
 
       {/*  Task Card */}
       <div className=" grid grid-cols-4 mx-auto gap-4">
@@ -282,9 +273,10 @@ function App({ URL }) {
     return (
       <div>
         {formModal && (
-          <div className="flex z-10 bg-black/50 ">
-            <form className="bg-white p-20">
+          <div className="flex z-10 bg-black/70 inset-0  fixed justify-center items-center">
+            <form className="bg-white p-10 w-1/2 rounded-lg">
               <div>
+                <p className="text-3xl font-bold">Edit</p>
                 <label>Task</label>
                 <input
                   type="text"
@@ -362,6 +354,7 @@ function App({ URL }) {
                 <button
                   className="bg-blue-700 hover:bg-blue-500 text-white px-2 py-1 rounded col-span-full"
                   type="submit"
+                  onClick={() => handleUpdate(formData)}
                 >
                   Update
                 </button>
