477f792a3ba2b61374c59d657b9957bfa5641c80
George Appianing <george.adu.appianing@gmail.com>
2025-05-26 23:13:28 +0000

    completed search functionality

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,22 +1,36 @@
 import React from "react";
-
+import { useState } from "react";
 export const Data = ({
-  //   todos,
   filteredSearch,
-
   handleDelete,
   handleEditInput,
   categoryColors,
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemPerPage = 8;
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  const currentItems = filteredSearch.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(filteredSearch.length / itemPerPage);
+
+  function prevPage() {
+    if (currentPage > 1) {
+      return setCurrentPage(currentPage - 1);
+    }
+  }
+  function nextPage() {
+    if (currentPage < totalPages) {
+      return setCurrentPage(currentPage + 1);
+    }
+  }
   return (
     <div className=" grid grid-cols-4 mx-auto gap-4">
-      {filteredSearch.map((todo) => (
+      {currentItems.map((todo) => (
         <div key={todo.id} className="bg-white shadow-sm rounded p-4 ">
           <h2 className="font-bold text-sm my-1">{todo.task}</h2>
           <p className="text-xs my-1">status: {todo.status}</p>
           <p className="text-xs my-1">Priority: {todo.priority}</p>
           <p className="text-xs my-4">{todo.notes}</p>
-
           <div className="flex justify-between mt-2">
             <span
               className={`text-xs px-2 py-1 rounded-full ${
@@ -44,6 +58,21 @@ export const Data = ({
           </div>
         </div>
       ))}
+      <div className="flex justify-between items-center mt-4">
+        <button
+          className="bg-blue-600 text-white rounded px-2 py-1"
+          onClick={prevPage}
+        >
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button
+          className="bg-blue-600 text-white rounded px-2 py-1"
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
